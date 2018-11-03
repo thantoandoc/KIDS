649debe01fd6ad889ff3e404a6e5af4a8680e986
Gracefully Broken <thantoandoc83@gmail.com>
2018-11-03 17:27:41 +0700

    muc trong video

--- routes/admin/video/index.js
+++ routes/admin/video/index.js
@@ -10,6 +10,21 @@ router.route('/danh-sach.html').get(function (req, res) {
 router.route('/').get(function (req, res) {
     res.redirect(rootPath + 'danh-sach.html');
 });
+router.route('/films.html').get(function (req, res) {
+    res.render(rootRender + 'danhsach',{title : "Hello"});
+});
+router.route('/game-videos.html').get(function (req, res) {
+    res.render(rootRender + 'danhsach',{title : "Hello"});
+});
+router.route('/kid-videos.html').get(function (req, res) {
+    res.render(rootRender + 'danhsach',{title : "Hello"});
+});
+router.route('/sport-videos.html').get(function (req, res) {
+    res.render(rootRender + 'danhsach',{title : "Hello"});
+});
+
+
+
 
 
 module.exports = router;
